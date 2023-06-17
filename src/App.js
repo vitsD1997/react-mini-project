ff60962c7576d6d51212d754b63bc9161465ed16
vitthal <dhakanevitthal30@gmail.com>
2023-06-17 16:42:52 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,247 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-12 text-center">
+            <h3 class="animate-charcter" style={{ marginTop: "50px" }}>
+              CDAC C-CAT Counselling and Seat Allotment Process
+            </h3>
+          </div>
+        </div>
+      </div>
+      <p class="container">
+        The Centre for Development of Advanced Computing (CDAC) will conduct the
+        C-CAT counselling and seat allotment process. The CDAC C-CAT counselling
+        and seat allotment will consist of two counselling sessions and three
+        rounds of seat allotments. Aspirants will have to fill their preferred
+        choices of PG Diploma courses and training centres during the CDAC C-CAT
+        counselling sessions. The first CDAC C-CAT counselling will start as per
+        schedule and candidates will be able to fill choices of course and
+        centres before the last date.
+      </p>
+      <br />
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-start">
+            <p>
+              <strong>Step 1:</strong> Visit the official website of CDAC.
+            </p>
+            <p>
+              <strong>Step 2:</strong> Provide the form number and password.
+            </p>
+            <p>
+              <strong>Step 3:</strong> Choose the choice filling option.
+            </p>
+            <p>
+              <strong>Step 4:</strong> Save the filled options.
+            </p>
+          </div>
+        </div>
+      </div>
+      <p class="container">
+        Candidates are advised to check the complete CDAC C-CAT counselling and
+        seat allotment schedule beforehand to keep a track on all the events.
+        The following table can be referred to check the CDAC C-CAT counselling
+        and seat allotment dates
+      </p>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h3 class="animate-charcter" style={{ marginTop: "50px" }}>
+              CDAC C-CAT Counselling and Seat Allotment Process
+            </h3>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h3 class="animate-charcter" style={{ marginTop: "50px" }}>
+              The following steps will be involved in the counselling and seat
+              allotment of CDAC C-CAT
+            </h3>
+          </div>
+        </div>
+      </div>
+      <p class="container">
+        Step 1:
+        <strong>
+          Online Course and Centres selection for 1st counselling session-
+        </strong>
+        Candidates will have to log in to the C-DAC website using their form
+        number and password and choose their preferences of courses and centres.
+        Candidates may give as many course-centre preferences as desired.
+        Candidates will be able to add or delete preferences and also change
+        their order of preferences before the last date. It must be noted that
+        the last-saved course- centre preferences will be considered as final
+        and a candidate could not opt for a lower preferred choice if he/she
+        will be allocated a higher preference in any round of seat allocation..
+      </p>
+      <p class="container">
+        Step 2: <strong>Declaration of First Round of Seat Allocation-</strong>{" "}
+        Candidates will have to check the course/centre allocated to them by
+        logging in on the C-DAC website. All the seat-allocated candidates will
+        also receive an email notification from the authorities. This seat
+        allocation will be done on the basis of CDAC C-CAT ranks of the
+        candidates and preferences set by them.
+      </p>
+      <p class="container">
+        Candidates allotted seats also have to pay the first installment fee of
+        Rs. 10,000+ GST in opline mode to confirm their allotted seats.
+        Candidates who are allotted seats in the first round and did not pay the
+        first installment will not be considered for the second round of CDAC
+        C-CAT seat allotment.
+      </p>
+      <p class="container">
+        <strong>Step 3: Declaration of Second Round of Seat Allocation-</strong>{" "}
+        Candidates who have already filled their choices of centres/courses but
+        were not offered seats in the first round of seat allocation and the
+        candidates who had been already allocated seats in the first round of
+        allocation, but did not get their top preferences, will be considered
+        for the second round of CDAC C-CAT seat allotment. Candidates who will
+        be allotted seats in this round have to pay thefirst installment in
+        online mode. Those who have already paid the fee in the first round were
+        not required to pay.
+      </p>
+      <p class="container">
+        <strong>
+          Step 4: Online Course and Centres selection for 2nd counselling
+          session-
+        </strong>{" "}
+        The second counselling of CDAC C-CAT will be done online only for the
+        vacant seats available at C-DAC's training centres. Candidates have to
+        login to the official website to check their centre/course eligibility
+        for second counseling based on the C-CAT cut-off rank of the
+        centre/course. Only the courses/centres that will be rank-wise eligible
+        for the candidate will be displayed in his/her list.
+      </p>
+      <p class="container">
+        <strong>Step 5: Third Round of CDAC C-CAT Seat Allotment-</strong> The
+        result of third seat allotment will be available on the CDAC website.
+        Candidates who are nos allotted seats in the third round will be
+        refunded with the full caution deposit of Rs. 10,000.
+      </p>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h3 class="animate-charcter" style={{ marginTop: "50px" }}>
+              Payment of Balance Course Fee
+            </h3>
+          </div>
+        </div>
+      </div>
+      <p class="container">
+        Candidates who confirmed their seats by paying the first installment
+        have to pay the balance course fee through NEFT.
+      </p>
+      <p class="container">
+        After the second installment payment, if candidates wish to know the
+        status of their payment, they need to contact their allocated centre
+        (where they have credited the second installment). Contact details of
+        all C-DAC centres are given in the last section of this Admission
+        Booklet.
+      </p>
+      <p class="container">
+        Candidates should enter the correct NEFT details, otherwise their
+        payment confimation may be delayed
+      </p>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h3 class="animate-charcter" style={{ marginTop: "50px" }}>
+              Cancellation or Refund
+            </h3>
+          </div>
+        </div>
+      </div>
+      <p class="container">
+        Candidates who will be found non-eligible for the allotted seat will not
+        be refunded the first installment fee. Candidates who wish to withdraw
+        their allotted seats will have to fill an online withdrawal form on the
+        official website. After submitting the form online, candidates will have
+        to print the withdrawal form, sign, scan and email it to
+        actssupport@odiac.in. Candidates should mention 'Request for withdrawal
+        from Online Diploma admission' as the subject of the email.
+      </p>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h3 class="animate-charcter" style={{ marginTop: "50px" }}>
+              CDAC C-CAT Counselling and Seat Allotment - Key Points
+            </h3>
+          </div>
+        </div>
+      </div>
+      <p class="container">
+        •The authorities conduct the CDAC C-CAT admission process through two
+        counselling sessions and three rounds of seat allotments.
+      </p>
+      <p class="container">
+        •During the CDAC C-CAT counselling sessions, candidates will have to
+        choose the courses and centres. The first and second round of CDAC C-CAT
+        seat allotment will be done on the basis of the first counselling
+      </p>
+      <p class="container">
+        •session. The third round of seat allotment will be done on the basis of
+        the second CDAC C-CAT counselling session.
+      </p>
+      <p class="container">
+        •<strong> Important Note:</strong> A candidate cannot join a lower
+        preferred choice if he/she is allocated a higher preference in any round
+        of seat allocation.
+      </p>
+      <p class="container">
+        •Depending on their C-CAT ranks, candidates will be allocated seats
+        strictly based on the order of preferences of courses and centres given
+        by them during the counselling
+      </p>
+      <p class="container">
+        •In order to participate in second counselling, candidates belonging to
+        the above-mentioned four categories have to pay a caution deposit of Rs
+        10,000/- + GST
+      </p>
+      <p class="container">
+        •In the second choice-filling, candidates should give preference to ony
+        centre/course only if they are keen on taking admission to it. Caution
+        deposit shall not be refunded to candidates who are allocated seats the
+        third round
+      </p>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h3 class="animate-charcter" style={{ marginTop: "50px" }}>
+              Frequently Asked Question (FAQs)
+            </h3>
+          </div>
+        </div>
+      </div>
+      <strong>
+        <p class="container">
+          Question: Is it compulsory to pay the first installment of fee after
+          CDAC C-CAT seat allotment Round 1 if I don't want the seat?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </strong>
+      <p class="container">
+        <strong>Answer:</strong> Yes, you will have to pay the first installment
+        of Rs 10000+ GST to confirm your allotted seat else you will lose the
+        seat. Also, candidates who do fee are not eligible to participate in the
+        second round of CDAC C-CAT seat allotment.
+      </p>
+      <strong>
+        <p class="container">
+          Question: Can lock a seat that has been allocated to me after the
+          first round of CDAC C-CAT seat allocation?
+        </p>
+      </strong>
+      <p class="container">
+        <strong>Answer:</strong> Candidates cannot lock the centre/seat
+        allocated to them after the first round of CDAC C-CAT 2022 seat
+        allocation unless the allotted seat wa preference. If the allotted seat
+        is not their first preference then it may get upgraded in the further
+        rounds.
+      </p>
+    </>
   );
 }
 
